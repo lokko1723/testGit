e499764fa5933dfd525d3205cfa79529d1cb3e69
IhorKashpruk <igork1823@gmail.com>
2020-10-15 21:22:42 +0200

    including feature.h file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "feature.h"
 
 using namespace std;
 
@@ -20,6 +21,6 @@ void loo() {
 
 int main(int argc, char const *argv[])
 {
-	
+	cout << "add(1+3) = " << add(1,3) << "\n";
 	return 0;
 }
